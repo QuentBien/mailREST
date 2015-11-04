8b18c8e32e0b6d5a9a8f063a5687ac5446936dd8
QuentBien <chauvel.quentin.95@gmail.com>
2015-11-04 11:05:49 +0100

    test rest non concluant

--- WEB-INF/classes/test/toast.java
+++ WEB-INF/classes/test/toast.java
@@ -1,4 +1,4 @@
-package rest;
+package test;
 
 import java.util.Date;
 import java.util.Properties;
@@ -12,19 +12,19 @@ public class toast {
 
 	public static void main(String[] args) throws NamingException {
 		//connexion au serveur glassfish
-		Properties properties = new Properties();
+		/*Properties properties = new Properties();
 		properties.put("java.naming.factory.initial","com.sun.enterprise.naming.SerialInitContextFactory");
 		properties.put("java.naming.factory.url.pkgs","com.sun.enterprise.naming");
 		properties.put("java.naming.factory.state","com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
 		properties.put("org.omg.CORBA.ORBInitialHost", "localhost");
 		properties.put("org.omg.CORBA.ORBInitialPort","3700"); //deuxième argument : port de glassfish
-		Context ctx = new InitialContext(properties);
-		IMessagerie toto = (IMessagerie) ctx.lookup("MessagerieBean");
+		*/
+		IMessagerie toto = (IMessagerie) new InitialContext().lookup("MessagerieBean");
 		System.out.println(toto.toString());
 		try {
-			toto.creerCompte("toto2", "TOTO2", "bien", new Date());
-			Compte c =toto.consulterCompte("toto2");
-			System.out.println(c);
+			//toto.creerCompte("toto2", "TOTO2", "bien", new Date());
+			Compte c = toto.consulterCompte("toto2");
+			System.out.println(c.getLogin());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
