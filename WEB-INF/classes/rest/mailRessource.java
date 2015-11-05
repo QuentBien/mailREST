ea45357def5cfc939541667785fc5e95e4bce600
QuentBien <chauvel.quentin.95@gmail.com>
2015-11-05 15:01:52 +0100

    tests : creercompte fonctionne, consultercompte partiel (sous glassfish)

--- WEB-INF/classes/rest/mailRessource.java
+++ WEB-INF/classes/rest/mailRessource.java
@@ -1,12 +1,20 @@
 package rest;
 
+import java.util.Date;
 import java.util.Properties;
 
 import javax.naming.InitialContext;
-import javax.naming.NamingException;
+import javax.naming.*;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+/*
+import org.glassfish.internal.api.Globals;
 
+import com.sun.appserv.jdbc.DataSource;
+import com.sun.enterprise.module.ModulesRegistry;
+import com.sun.enterprise.module.bootstrap.StartupContext;
+import com.sun.enterprise.module.single.StaticModulesRegistry;
+*/
 import ejb.entity.*;
 import ejb.service.IMessagerie;
 
@@ -15,16 +23,21 @@ public class mailRessource {
 	private static IMessagerie messagerie;
 
 	public mailRessource() {
+		
 		try {
 			//connexion au serveur glassfish
+			/*
 			Properties properties = new Properties();
 			properties.put("java.naming.factory.initial","com.sun.enterprise.naming.SerialInitContextFactory");
 			properties.put("java.naming.factory.url.pkgs","com.sun.enterprise.naming");
 			properties.put("java.naming.factory.state","com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
 			properties.put("org.omg.CORBA.ORBInitialHost", "localhost");
 			properties.put("org.omg.CORBA.ORBInitialPort","3700"); //deuxième argument : port de glassfish
-			mailRessource.setMessagerie((IMessagerie) new InitialContext(properties).lookup("ejb/messagerie"));
+			*/
+			mailRessource.setMessagerie((IMessagerie) new InitialContext().lookup("MessagerieBean"));
+			
 		} catch (NamingException e) {e.printStackTrace();}
+		
 	}
 
 	@Path("/compte")
@@ -40,18 +53,31 @@ public class mailRessource {
 
 	}
 
-	@Path("/{login}")
+	@Path("/compte/tutu/{login}")
 	@GET
-	@Produces ({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	@Produces (MediaType.APPLICATION_JSON)
 	public Compte consulterCompte(@PathParam("login") String login) {
 		Compte c = null;
+		String s = "{\"bien\":\"cool\"}";
 		try {
+			c = new Compte("lui", "bien", "Quent", new Date());
 			c = mailRessource.getMessagerie().consulterCompte(login);
+			String t = c.getLogin();
+			s = "{\"login\":\"" + t +"\",";
+			s += "\"messagerie\":\"" + mailRessource.getMessagerie().toString() +"\"}";
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
+			s = "{\"exception\":\"" + e.toString() + "\"}";
 			e.printStackTrace();
 		}
+		//return s;
 		return c;
+	}
+	
+	@GET
+	@Path("/compte/test/{compte}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Compte avoirCompte(@PathParam("compte") String compte) {
+		return new Compte(compte, "bien", "Quent", new Date());
 	}
 	
 	@Path("/message")
@@ -61,7 +87,12 @@ public class mailRessource {
 		mailRessource.getMessagerie().envoyerMessage(m.getObjet(), m.getCorps(), m.getEmetteur().getLogin(), m.getDestinataire().getLogin());
 	}
 	
-
+	@GET
+	@Path("/accueil/{nomPromo}")
+	@Produces(MediaType.APPLICATION_XML)
+	public String direBonjour(@PathParam("nomPromo") String promo) {
+		return "<accueil> Bonjour la promo de : " + promo + " ! </accueil>";
+	}
 	
 	public static IMessagerie getMessagerie() {
 		return messagerie;
--- WEB-INF/classes/test/toast.java
+++ WEB-INF/classes/test/toast.java
@@ -12,19 +12,19 @@ public class toast {
 
 	public static void main(String[] args) throws NamingException {
 		//connexion au serveur glassfish
-		/*Properties properties = new Properties();
+		Properties properties = new Properties();
 		properties.put("java.naming.factory.initial","com.sun.enterprise.naming.SerialInitContextFactory");
 		properties.put("java.naming.factory.url.pkgs","com.sun.enterprise.naming");
 		properties.put("java.naming.factory.state","com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
 		properties.put("org.omg.CORBA.ORBInitialHost", "localhost");
 		properties.put("org.omg.CORBA.ORBInitialPort","3700"); //deuxième argument : port de glassfish
-		*/
-		IMessagerie toto = (IMessagerie) new InitialContext().lookup("MessagerieBean");
+		Context ctx = new InitialContext(properties);
+		IMessagerie toto = (IMessagerie) ctx.lookup("MessagerieBean");
 		System.out.println(toto.toString());
 		try {
 			//toto.creerCompte("toto2", "TOTO2", "bien", new Date());
-			Compte c = toto.consulterCompte("toto2");
-			System.out.println(c.getLogin());
+			Compte c = toto.consulterCompte("moi");
+			System.out.println(c.getSignup());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
